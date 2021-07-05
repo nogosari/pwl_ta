7c2bfc76ca0e35ad784ad7710f80dddc94780324
nogosari <littlebadboy1945@gmail.com>
2021-07-05 13:33:19 +0700

    update fitur

--- classes/connection.php
+++ classes/connection.php
@@ -4,7 +4,7 @@ class Connection
   private $host = 'localhost';
   private $db = 'jadwal';
   private $user = 'nogosari';
-  private $pass = 'mend';
+  private $pass = '';
 
   private $pdo = null;
 
